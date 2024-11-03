39b12861fe3572c062aa390decf38ba204dc7f7d
mengfei <122935692+feifeihung@users.noreply.github.com>
2024-11-02 22:47:18 -0400

    Update capstone_dashboard_v2.py

--- capstone_dashboard_v2.py
+++ capstone_dashboard_v2.py
@@ -365,7 +365,7 @@ st.markdown("""
 
 with st.sidebar:
     st.markdown("# Select a Sector")  # Header in sidebar
-    page = st.radio(['Data Visualization', 'Prediction'])
+    page = st.radio('',['Data Visualization', 'Prediction'])
 
 
 
